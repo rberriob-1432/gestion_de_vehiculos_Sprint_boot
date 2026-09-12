ded7795e12de5b0fb7ce24cf72fee45548400d45
rberriob-1432 <rberriob@unicartagena.edu.co>
2026-09-12 18:28:50 -0500

    Implementa servicios de consulta de usuarios

--- src/main/java/com/rabb/usuariomanejo/applicacion/puertos/entrada/ConseguirTodosUsuariosUsoCaso.java
+++ src/main/java/com/rabb/usuariomanejo/applicacion/puertos/entrada/ConseguirTodosUsuariosUsoCaso.java
@@ -1,6 +1,5 @@
 package com.rabb.usuariomanejo.applicacion.puertos.entrada;
 import com.rabb.usuariomanejo.dominio.modelo.UsuarioModelo;
 import java.util.List;
-public interface ConseguirTodosUsuariosUsoCaso {
-    List<UsuarioModelo> execute();
-}
+public interface ConseguirTodosUsuariosUsoCaso
+{ List<UsuarioModelo> execute(); }
